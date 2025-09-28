e8bdd77f321fd3a6f9668382e5f325219914fdf9
Exc1usiveRUS <shark100494@yandex.ru>
2025-09-28 21:12:29 +0300

    правки помника, попытка №?

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/repository/ShoppingCartRepository.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/repository/ShoppingCartRepository.java
@@ -10,5 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, UUID> {
-    Optional<ShoppingCart> findByUsernameAndState(String username, ShoppingCartState state);
+    Optional<ShoppingCart> findByUserNameAndState(String username, ShoppingCartState state);
 }
